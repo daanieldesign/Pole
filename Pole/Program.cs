30629d7ab5987305c55c2a83f59d7ff48d6b94fe
daanieldesign <mahdakdaniel@gmail.com>
2023-06-08 13:16:33 +0200

    update

--- Pole/Program.cs
+++ Pole/Program.cs
@@ -10,7 +10,39 @@ namespace Pole
     {
         static void Main(string[] args)
         {
+            //Pole je datová struktura, která obsahuje hodnoty stejného datového typu
+            //Na pole můžeme nahlížet jako seznam buněk, které mají svoji pozici a hodnoty
+            //Velikost pole za běhu programu je neměnná
+            //Praktické použití – uložení čísel do řady, práce s textem, vyhledávání dat
 
+            int[] pole = new int[5]; //datový typ [] název = new int [velikost]
+            int[] cisla = { 10, 78, -2, 36 }; //Vložení hodnot při incializaci pole
+
+            Console.WriteLine("Stav pole před změnou");
+            Console.WriteLine(cisla[0]);
+            Console.WriteLine(cisla[2]);
+
+            cisla[2] = 100; //Přepsání hodnoty na indexu dva
+            cisla[0] = -100;
+
+            //Indexování pole začíná od nuly, index představuje uloženou pozici prvku
+            Console.WriteLine("Stav pole po změně");
+            Console.WriteLine(cisla [0]);
+            Console.WriteLine(cisla [2]);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(pole[1]); //Každý průchod zobrazí obsah indexu pole
+            }
+
+            for (int i = 0; i < cisla.Length; i++) //Příkaz Length zjistí počet elementů pole
+            {
+                Console.WriteLine(cisla [i]);
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
